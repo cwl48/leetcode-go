e2bcaa07edc761c5e2b76803e4b762dca085983a
cwl48 <1159849884@qq.com>
2019-02-11 16:52:46 +0800

    48

---
+++ 48/rotate-image_test.go
@@ -0,0 +1,20 @@
+package rotate48
+
+import "testing"
+
+func Test_rotate(t *testing.T) {
+	type args struct {
+		matrix [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{"test", args{[][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rotate(tt.args.matrix)
+		})
+	}
+}
