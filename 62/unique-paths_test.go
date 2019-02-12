c790a9db5fdcbb804292aef891191ecb4aef3072
cwl48 <1159849884@qq.com>
2019-02-12 14:44:13 +0800

    62

---
+++ 62/unique-paths_test.go
@@ -0,0 +1,26 @@
+package uniquepaths62
+
+import "testing"
+
+func Test_uniquePaths(t *testing.T) {
+	type args struct {
+		m int
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"test", args{7, 3}, 28},
+		{"test", args{7, 3}, 28},
+
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := uniquePaths(tt.args.m, tt.args.n); got != tt.want {
+				t.Errorf("uniquePaths() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
